d30142b48c20f4496c14265b9c4a7675a4003a21
V-Sentry Teams <maheza.firzadillah@compnet.co.id>
2026-03-15 11:56:08 +0700

    upload

---
+++ upload.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Mengunggah perubahan ke GitHub...
+git add .
+set /p msg="Masukkan pesan commit (kosongkan untuk default): "
+if "%msg%"=="" set msg="Update program via batch script"
+git commit -m "%msg%"
+git push origin main
+echo Selesai! Perubahan sudah ada di GitHub.
+pause
